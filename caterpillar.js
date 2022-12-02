8fdfa315a0ca0ba40f15f95d071b2d703fbd787a
CarolineDCCC <100736960+CarolineDCCC@users.noreply.github.com>
2022-12-02 00:25:16 -0500

    Add files via upload

---
+++ caterpillar.js
@@ -0,0 +1,41 @@
+"use strict"
+
+
+
+$(function() {  
+    $('#restore').hide();
+    
+    $("#transform").on('click', function() {
+        // Add your code here
+        $("h1").text("The Butterfly");
+        $("#p1").html("This is a butterfly in its natural habitat");
+        $("img").attr("src", "https://www.kasandbox.org/programming-images/animals/butterfly_monarch.png");
+        $("h1").css("color", "orange");
+        var newUL = $("<ul>");
+        $("ul").insertAfter($("a"));
+        
+        
+        newUL.append("<li>Butterflies are insects from the order Lepidoptera, which also includes moths.</li>");
+        newUL.append("<li>Butterfly fossils date to the Paleocene, about 56 million years ago.</li>");
+        newUL.append("<li>Butterflies use their antennae to sense the air for wind and scents.</li>");
+        newUL.append("<li>Butterflies have appeared in art from 3500 years ago in ancient Egypt.</li>");
+        $("main").append(newUL);
+        
+        $("a").attr("href", "https://en.wikipedia.org/wiki/Butterfly");
+        $("a").text("Butterfly");
+        newUL.addClass("result");
+        $("#p1").addClass("butterfly");
+        $("#restore").insertAfter($("ul"));
+        
+        
+
+    $('#restore').show();
+    $("#transform").hide();
+    });
+
+    $("#restore").on('click', function() {
+        location.reload(true); 
+    }); 
+
+});
+
